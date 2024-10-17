fcdb838975288e572fa434ac81714cfc8c6bf12e
rishavnathpati <rishav@convai.com>
2024-10-17 22:28:57 +0530

    Refactor: Remove unused namespaces from scripts

--- Assets/Scripts/NPC_Friendly.cs
+++ Assets/Scripts/NPC_Friendly.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class NPC_Friendly : MonoBehaviour
 {
